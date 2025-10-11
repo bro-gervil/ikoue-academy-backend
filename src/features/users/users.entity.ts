bf2a6e940c1e9a2c0b62455b1064eaf91d4748a9
Titus <titus@gahissy.com>
2025-10-11 12:13:10 +0100

    chore: refactoring

---
+++ src/features/users/users.entity.ts
@@ -0,0 +1,36 @@
+export type UserEntity = {
+	id: string;
+	email: string;
+	password: string;
+	firstName: string;
+	lastName: string;
+	role: string; // admin, client, technician
+	createdAt: Date;
+	updatedAt: Date;
+};
+
+export interface UserRepository {
+	create: (user: UserEntity) => Promise<UserEntity>;
+	findAll: () => Promise<UserEntity[]>;
+	findById: (id: string) => Promise<UserEntity | undefined>;
+	update: (user: UserEntity) => Promise<UserEntity>;
+	delete: (id: string) => Promise<void>;
+}
+
+export type CreateUserInput = {
+	email: string;
+	password: string;
+	firstName: string;
+	lastName: string;
+	role: string; // admin, client, technician
+};
+
+export type CreateUserOutput = {
+	id: string;
+	email: string;
+	firstName: string;
+	lastName: string;
+	role: string; // admin, client, technician
+	createdAt: Date;
+	updatedAt: Date;
+};
